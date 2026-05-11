0ad2af612b9f6df073bebd5fed61a943ea996008
~KAROL <karolynekschaves@gmail.com>
2026-05-11 00:29:33 -0300

    Add files via upload

---
+++ RachaAi/Models/RachaAi.cs
@@ -0,0 +1,17 @@
+namespace RachaAi.Models
+
+{
+
+
+public class Grupos
+{
+    public string Name { get; set; } = string.Empty;
+    public string Categoria { get; set; } = string.Empty;
+
+    public decimal ValorPendente { get; set; }
+
+    public bool NoVermelho { get; set; }
+  }
+}
+
+
